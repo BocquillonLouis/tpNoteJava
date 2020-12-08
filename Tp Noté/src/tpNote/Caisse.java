07f2202c23fa5e424031dbfa5b7b91f8cc6bf971
BocquillonLouis <louis.bocquillonliger-belair@iut-rodez.fr>
2020-12-08 11:59:47 +0100

    Ajout de commentaires

--- Tp Noté/src/tpNote/Caisse.java
+++ Tp Noté/src/tpNote/Caisse.java
@@ -27,29 +27,44 @@ public class Caisse {
 
 	// Getters et setters
 	
+	/**
+	 * Retourne un boolean indiquant si la caisiière est expérimentée ou non
+	 * @return Un boolean indiquant si la caisiière est expérimentée ou non
+	 */
 	public boolean isCaissiereExperimentee() {
 		return caissiereExperimentee;
 	}
 
+	/**
+	 * Changer la valeur du boolean indiquand si la caisiière est expérimentée ou non
+	 * @param caissiereExperimentee Un boolean indiquant si la caisiière est expérimentée ou non
+	 */
 	public void setCaissiereExperimentee(boolean caissiereExperimentee) {
 		this.caissiereExperimentee = caissiereExperimentee;
 	}
 
+	/**
+	 * Retourne la liste des clients de cette caisse
+	 * @return la liste des clients de cette caisse
+	 */
 	public ConcurrentLinkedQueue<Client> getFileClients() {
 		return fileClients;
 	}
 
-	public void setFileClients(ConcurrentLinkedQueue<Client> fileClients) {
-		this.fileClients = fileClients;
-	}
-
+	/**
+	 * Retourne la durée de traitement des caddies de tous les clients
+	 * @return La durée de traitement des caddies de tous les clients de cette caisse
+	 */
 	public int getDureeTraitementCaisse() {
 		return dureeTraitementCaisse;
 	}
 
+	/**
+	 * changer la durée de traitement des caddies de tous les clients
+	 * @param La durée de traitement des caddies de tous les clients de cette caisse
+	 */
 	public void setDureeTraitementCaisse(int dureeTraitementCaisse) {
 		this.dureeTraitementCaisse = dureeTraitementCaisse;
 	}
-	
 	
 }
--- Tp Noté/src/tpNote/Client.java
+++ Tp Noté/src/tpNote/Client.java
@@ -20,18 +20,34 @@ public class Client {
 	
 	// getters and setters
 
+	/**
+	 * Retourne la date d'arrivée du client
+	 * @return La date d'arrivée du client
+	 */
 	public int getDateArrivee() {
 		return dateArrivee;
 	}
 
+	/**
+	 * Changer la date d'arrivée du client
+	 * @param dateArrivee La date d'arrivée du client
+	 */
 	public void setDateArrivee(int dateArrivee) {
 		this.dateArrivee = dateArrivee;
 	}
 
+	/**
+	 * Retourne la durée de traitement du caddie du client
+	 * @return La date durée de traitement du caddie du client
+	 */
 	public int getDureeTraitementCaddie() {
 		return dureeTraitementCaddie;
 	}
 
+	/**
+	 * Changer la durée de traitement du caddie du client
+	 * @param dureeTraitementCaddie La date durée de traitement du caddie du client
+	 */
 	public void setDureeTraitementCaddie(int dureeTraitementCaddie) {
 		this.dureeTraitementCaddie = dureeTraitementCaddie;
 	}
